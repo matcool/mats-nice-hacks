8b1552b277fa625b89f4967424cf5bafec0048f2
matcool <26722564+matcool@users.noreply.github.com>
2022-06-02 20:10:40 -0300

    move some code

--- src/state.cpp
+++ src/state.cpp
@@ -12,6 +12,44 @@ auto get_save_path() {
 	return get_exe_path() / "mathacks.txt";
 }
 
+template <size_t N, class T>
+void load_schema_loop(T& obj, const std::unordered_map<std::string, std::string>& values) {
+	using S = get_schema<T>;
+	auto f = values.find(S::names[N]);
+	if (f != values.end()) {
+		const auto str = f->second; 
+		using type = typename S::template type_at<N>;
+		auto& value = S::template value_at<N>(obj);
+		if constexpr (std::is_same_v<type, bool>) {
+			value = str == "true";
+		} else if constexpr (std::is_same_v<type, float>) {
+			value = std::stof(str);
+		} else {
+			static_assert(!std::is_same_v<type, type>, "unsupported type");
+		}
+	}
+	if constexpr (N < S::size() - 1)
+		load_schema_loop<N + 1>(obj, values);
+}
+
+template <size_t N, class T>
+void save_schema_loop(const T& obj, std::ofstream& file) {
+	using S = get_schema<T>;
+	using type = typename S::template type_at<N>;
+	const auto& value = S::template value_at<N>(obj);
+	file << S::names[N] << '=';
+	if constexpr (std::is_same_v<type, bool>) {
+		file << (value ? "true" : "false");
+	} else if constexpr (std::is_same_v<type, float>) {
+		file << value;
+	} else {
+		static_assert(!std::is_same_v<type, type>, "unsupported type");
+	}
+	file << std::endl;
+	if constexpr (N < S::size() - 1)
+		save_schema_loop<N + 1>(obj, file);
+}
+
 void StateStruct::load() {
 	const auto path = get_save_path();
 	if (std::filesystem::exists(path)) {
@@ -23,12 +61,12 @@ void StateStruct::load() {
 				values.insert(split_once(line, '='));
 		}
 
-		load_schema_loop<0>(this, values);
+		load_schema_loop<0>(*this, values);
 	}
 }
 
 void StateStruct::save() {
 	std::ofstream file(get_save_path());
 
-	save_schema_loop<0>(this, file);
+	save_schema_loop<0>(*this, file);
 }
